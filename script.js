0d5d1a2e4fdc23a390f8154ff9f4e88f65c88140
LancharroDav <davidglancharro@gmail.com>
2020-03-13 17:21:16 +0100

    JS fixed and html&css updated

--- script.js
+++ script.js
@@ -1,43 +1,68 @@
-let playerChoice = playerPlay()
-let computerChoice = computerPlay()
-
-
-function playerPlay(clicked_id) {
-    return clicked_id
-    
+// let playerChoice = ''
+// let computerChoice = 
+let round = ''
+let playerScore = 0
+let computerScore = 0
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
 }
 
-function computerPlay() {
-    const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
-    return choices[Math.floor(Math.random() * choices.length)];
+
+
+document.addEventListener('DOMContentLoaded', function(){ 
+console.log('document loaded')
+document.getElementById("rock").addEventListener('click', playerPlay)
+document.getElementById("paper").addEventListener('click', playerPlay)
+document.getElementById("scissors").addEventListener('click', playerPlay)
+document.getElementById("lizard").addEventListener('click', playerPlay)
+document.getElementById("spock").addEventListener('click', playerPlay)
+
+}, false);
+
+function playerPlay() {
+    let playerChoice = capitalize(this.id)
     
-}
-
-function playRound(playerChoice, computerChoice) {
-    let playerSelection =  playerChoice;
-    let computerSelection = computerChoice;
-
-    if (computerSelection == playerSelection) {
-        return "Draw! You've both chosen ${playerSelection}! Try again!!"
-    } else if (
-        computerSelection == 'rock' && playerSelection == 'lizard' ||
-        computerSelection == 'rock' && playerSelection == 'scissors' ||
-        computerSelection == 'paper' && playerSelection == 'rock' ||
-        computerSelection == 'paper' && playerSelection == 'spock' ||
-        computerSelection == 'scissors' && playerSelection == 'paper' ||
-        computerSelection == 'scissors' && playerSelection == 'lizard' ||
-        computerSelection == 'lizard' && playerSelection == 'paper' ||
-        computerSelection == 'lizard' && playerSelection == 'spock' ||
-        computerSelection == 'spock' && playerSelection == 'scissors' ||
-        computerSelection == 'spock' && playerSelection == 'rock'
-    ) {
-        return "You win! ${playerSelection} will always beat ${computerSelection}!!"
-    } else {
-        return "Loser! ${computerSelection} will always beat ${playerSelection}!! AI will always be smarter than a mere human!"
+    function computerPlay() {
+        const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+        return choices[Math.floor(Math.random() * choices.length)]     
     }
+    let computerChoice = capitalize(computerPlay())
+
+    function playRound() {
+        const draw = "Draw! You've both chosen " + playerChoice + "! Try again!!"
+        const win = "You win! " + playerChoice + " will always beat " + computerChoice + "!! You lucky!"
+        const loose = "Loser! " + computerChoice + " will always beat " + playerChoice + "!! A.I. will always be smarter than a mere human!"
+
+        if (computerChoice === playerChoice) return draw;
+        if (playerChoice == 'Rock' && computerChoice == 'Lizard') return win;
+        if (playerChoice == 'Rock' && computerChoice == 'Scissors') return win;
+        if (playerChoice == 'Paper' && computerChoice == 'Rock') return win;
+        if (playerChoice == 'Paper' && computerChoice == 'Spock') return win;
+        if (playerChoice == 'Scissors' && computerChoice == 'Paper') return win;
+        if (playerChoice == 'Scissors' && computerChoice == 'Lizard') return win;
+        if (playerChoice == 'Lizard' && computerChoice == 'Paper') return win;
+        if (playerChoice == 'Lizard' && computerChoice == 'Spock') return win;
+        if (playerChoice == 'Spock' && computerChoice == 'Scissors') return win;
+        if (playerChoice == 'Spock' && computerChoice == 'Rock') return win;
+        else {
+            return loose
+        }
+    }
+    console.log(playerChoice)
+    console.log(computerChoice)
+    console.log(playRound())
+    document.getElementById("gameMessage").innerHTML = playRound()
 }
+
+
+
+
 
 function game() {
-    let playGame = playRound()
-    document.getElementById("gameMessage").innerHTML = playGame
+    document.getElementById("gameMessage").innerHTML = playerPlay()
 }
+
+// function score(round) {
+    
+// }
